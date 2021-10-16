91cfd306b98e681ad3d29d92c677ecb373713706
Bilgin <kocakbilgin@gmail.com>
2021-10-16 21:29:49 +0300

    commit 11

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
+application = FastAPI()
 
 
-@app.get("/")
+@application.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(application, host="0.0.0.0", port=8000)
